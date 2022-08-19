2c14e5b57d453298a7521e74650a2765b820a67c
Raz <razman786@gmail.com>
2022-08-19 10:41:41 +0100

    Updated mem test

--- mem_test/run.sh
+++ mem_test/run.sh
@@ -1,4 +1,8 @@
-export OMP_NUM_THREADS=`lscpu | grep -w '^CPU(s):'|awk '{print $2}'`
+cpu_num=`lscpu | grep -w '^CPU(s):'|awk '{print $2}'`
+export OMP_NUM_THREADS=$cpu_num
+# optionally set OMP affinity
+# ((max_aff=$cpu_num-1))
+# export GOMP_CPU_AFFINITY=0-$max_aff
 export KMP_AFFINITY=compact
 # use if HT enabled
 # export KMP_AFFINITY=granularity=fine,compact,1,0
